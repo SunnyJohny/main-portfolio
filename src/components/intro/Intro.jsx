2f50980c9ac755de1cb81516a8ca6f2c56f67f13
john sunday <84939198+SunnyJohny@users.noreply.github.com>
2023-09-10 16:36:26 +0100

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -14,7 +14,7 @@ const Intro = () => {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["A Web Developer", "A Freelancer", "An App Developer"],
+      strings: [ "A Fullstack Developer" "Web Developer", "A Freelancer", "A Desktop App Developer", "A Mobile App Developer"],
     });
   }, []);
 
